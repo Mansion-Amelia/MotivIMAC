453523410a08c66b5efbf3b778852c96c69fd755
Mansion-Amelia <amelia.mansion@gmail.com>
2019-05-25 22:08:58 +0200

    Update login.php

--- app/auth/login.php
+++ app/auth/login.php
@@ -2,10 +2,6 @@
     include('../../app.php');
     require_once($link_app.'user.php');
     if(login_user()){
-        
-            /* Forget the inputs (TO ADD : if we check a checkbox "remember me", the inputs are saved anyway)*/
-            /*$_SESSION["login_username_user"]=NULL;
-            $_SESSION["login_password_user"]=NULL;*/
             header('Location: ../../layouts/home.php'); 
     }else{
             header('Location: form_login.php');
