7be0521eac403b5526bc7ca255d832870f44e65f
ManonSgro <manon-sgro@outlook.fr>
2019-05-25 23:11:17 +0200

    Correction order by taches

--- app/task.php
+++ app/task.php
@@ -12,7 +12,8 @@ function read_task_brief(){
     $request= "SELECT * FROM task, category, difficulty
     WHERE task.id_user='".$id_user."'
     AND task.id_category=category.id_category
-    AND task.id_difficulty=difficulty.id_difficulty";
+    AND task.id_difficulty=difficulty.id_difficulty
+    ORDER BY task.end_task";
     $result = $pdo->query($request) or die ("Erreur : la connexion a échoué.");
     
     if($result->rowCount()<1){
