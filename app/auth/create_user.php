c3a4ef720e210f7aa514e1081cfb365fcc970c31
Mansion-Amelia <amelia.mansion@gmail.com>
2019-05-25 22:06:01 +0200

    supp com inutiles

--- app/auth/create_user.php
+++ app/auth/create_user.php
@@ -3,15 +3,6 @@ include('../../app.php');
 
     require_once($link_app.'user.php');
     if(create_user()){
-        
-        /* Forget the inputs (TO ADD : if we check a checkbox "remember me", the inputs are saved anyway)*/
-        /*$_SESSION["lastname_user"]=NULL;
-        $_SESSION["firstname_user"]=NULL;
-        $_SESSION["email_user"]=NULL;
-        $_SESSION["mobile_user"]=NULL;
-        $_SESSION["username_user"]=NULL;
-        $_SESSION["password_user"]=NULL;
-        $_SESSION["id_universe"]=NULL;*/
         header('Location: form_login.php'); 
     }else{
         header('Location: form_user.php'); 
