41b1d57231d3697065863c4b57c092d1db14fbec
ManonSgro <manon-sgro@outlook.fr>
2019-05-25 13:43:40 +0200

    Correction header

--- layouts/task/form_task.php
+++ layouts/task/form_task.php
@@ -3,25 +3,12 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>MotivIMAC</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="../../css/sb-admin-2.min.css" rel="stylesheet">
-
+    <?php
+        include($link_partials.'header.php');
+    ?>
 </head>
 
 <body id="page-top">
@@ -40,18 +27,6 @@
 
       <!-- Main Content -->
       <div id="content">
-
-        <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-
-          <!-- Topbar Navbar -->
-          <ul class="navbar-nav ml-auto">
-            <a href="../../app/auth/logout.php">Deconnexion</a>
-          </ul>
-
-        </nav>
-        <!-- End of Topbar -->
 
         <?php
         require_once($link_app.'task.php');
